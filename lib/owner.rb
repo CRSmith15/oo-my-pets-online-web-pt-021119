d504abcd199656e80e7a460376c4e192531f3de8
Chance Smith <chancesmith97@hotmail.com>
2019-03-30 21:25:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,6 +43,14 @@ class Owner
   end
   
   def walk_dogs
-    @@pets[:dogs].each 
+    @@pets[:dogs].each {|dog| dog.mood = "happy"}
+  end 
+  
+  def play_with_cats
+    @@pets[:cats].each {|cat| cat.mood = "happy"}
+  end 
+  
+  def feed_fish
+    @@pets[:fishes].each {|fish| fish.mood = "happy"}
   end 
 end
