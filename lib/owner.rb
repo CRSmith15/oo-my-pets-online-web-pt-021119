8c6fa3e4ee3dfdeaf7d8823f8b4532fb14a20f3b
Chance Smith <chancesmith97@hotmail.com>
2019-03-30 21:36:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,12 +2,12 @@ class Owner
   attr_reader :species
   attr_accessor :name, :cat, :dog, :fish, :pets 
   @@all = []
-  @@pets = {:cats => [], :dogs => [], :fishes => []}
   
   def initialize(species)
     @species = species
     @name = name 
     @@all << self 
+    @pets = {:cats => [], :dogs => [], :fishes => []}
   end 
   
   def say_species
@@ -27,35 +27,35 @@ class Owner
   end
   
   def pets 
-    @@pets
+    @pets
   end
   
   def buy_fish(fish)
-    @@pets[:fishes] << Fish.new(fish)
+    @pets[:fishes] << Fish.new(fish)
   end
   
   def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
+    @pets[:cats] << Cat.new(cat)
   end
   
   def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
+    @pets[:dogs] << Dog.new(dog)
   end
   
   def walk_dogs
-    @@pets[:dogs].each {|dog| dog.mood = "happy"}
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
   end 
   
   def play_with_cats
-    @@pets[:cats].each {|cat| cat.mood = "happy"}
+    @pets[:cats].each {|cat| cat.mood = "happy"}
   end 
   
   def feed_fish
-    @@pets[:fishes].each {|fish| fish.mood = "happy"}
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
   end 
   
   def sell_pets
-    pets.each do |pet, array|
+    @pets.each do |pet, array|
       array.each do |pet|
         pet.mood = "nervous"
       end 
